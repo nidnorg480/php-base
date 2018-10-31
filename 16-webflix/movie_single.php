1ceb3bbeabfe720ba76c53935c1d90fe33b4d4a2
Matthieu Mota <matthieumota@gmail.com>
2018-10-31 12:27:38 +0100

    Begin single movie

---
+++ 16-webflix/movie_single.php
@@ -0,0 +1,14 @@
+<?php
+// We need a amazing movie helper
+require_once(__DIR__.'/functions/movie.php');
+
+// Le fichier header.php est inclus sur la page
+require_once(__DIR__.'/partials/header.php');
+
+?>
+
+
+
+<?php
+// Le fichier footer.php est inclus sur la page
+require_once(__DIR__.'/partials/footer.php'); ?>
